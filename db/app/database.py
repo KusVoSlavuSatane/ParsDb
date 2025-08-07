174a219c8239782e5de27d368382e1308d5c3e89
Pivo Gumengo <80218586+KusVoSlavuSatane@users.noreply.github.com>
2025-08-07 18:52:28 +0300

    Add files via upload

---
+++ db/app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import DATABASE_URL
+
+Base = declarative_base()
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_db() -> AsyncSession:
+    async with async_session() as session:
+        yield session
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
